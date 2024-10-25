fb0f1fcc873ec60a405ba24f54f9a6a44f8f7149
Perfi <152892303+raphaelperfi@users.noreply.github.com>
2024-10-25 17:58:52 -0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,19 +1,19 @@
 const solutions = [
   {
     name: "QR code component",
-    img: "../qr-code-component/design/desktop-design.jpg",
+    img: "./qr-code-component/design/desktop-design.jpg",
     deploy: "https://perfidev.github.io/frontend-mentor/qr-code-component",
     repo: "https://github.com/perfidev/frontend-mentor/tree/main/qr-code-component",
   },
   {
     name: "Blog preview card",
-    img: "../blog-preview-card/design/desktop-design.jpg",
+    img: "./blog-preview-card/design/desktop-design.jpg",
     deploy: "https://perfidev.github.io/frontend-mentor/blog-preview-card",
     repo: "https://github.com/perfidev/frontend-mentor/tree/main/blog-preview-card",
   },
   {
     name: "Social links profile",
-    img: "../social-links-profile/design/destkop-design.jpg",
+    img: "./social-links-profile/design/destkop-design.jpg",
     deploy: "https://perfidev.github.io/frontend-mentor/social-links-profile",
     repo: "https://github.com/perfidev/frontend-mentor/tree/main/social-links-profile",
   },
